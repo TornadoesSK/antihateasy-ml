5ca00b0f90c04742f3d9d846b41241f78821de77
Michal Demko <michal.demko@recombee.com>
2022-04-24 00:43:01 +0200

    implemented basic error message

--- src/web/endpoints.py
+++ src/web/endpoints.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, abort
 import json
 
 
@@ -11,6 +11,10 @@ def home():
 @endpoints.route('/gettextsentiment', methods=['POST'])
 def gettextsentiment():
     data = json.loads(request.data)
+    # if a username isn't supplied in the request, return a 400 bad request
+    if "tweet" not in data.keys():
+        abort(400)
+        
     if "you are a very bad ugly person" in data["tweet"]:
         return { "hide" : True }
     
