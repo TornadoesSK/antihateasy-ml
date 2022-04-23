6bfe071b3769b36b9b8dcf261baf94f2c82c179e
Michal Demko <michal.demko@recombee.com>
2022-04-23 17:09:12 +0200

    implemented basic text endpoint

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ with open('config.json') as f:
 app = create_app(config)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=config["PORT"])
--- src/web/endpoints.py
+++ src/web/endpoints.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, request
+import json
+
 
 endpoints=Blueprint('endpoints',__name__)
 
 @endpoints.route('/', methods=['GET'])
 def home():
     return "Hello world"
+
+@endpoints.route('/gettextsentiment', methods=['POST'])
+def gettextsentiment():
+    data = json.loads(request.data)
+    return {}
